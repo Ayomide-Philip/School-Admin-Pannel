60ba6ca4718fefe420fb802cd28df399ed5d5200
A-pilip <areoayomide2008@gmail.com>
2024-09-19 16:31:59 +0100

    updating my first script

--- script/script.js
+++ script/script.js
@@ -3,13 +3,12 @@ setInterval(getTime, 1000);
 function getTime() {
   const date = new Date();
   var seconds = date.getSeconds();
-  console.log(seconds.length);
-  
-  //   if (seconds.length === 1) {
-  //     console.log("true");
-  //   } else {
-  //     console.log("false");
-  //   }
+
+  if (seconds.length < 10) {
+    seconds = `0${seconds}`;
+  } else {
+    seconds = `${seconds}`;
+  }
   document.getElementById(
     "current-time"
   ).innerHTML = `${new Date().getHours()}:${new Date().getMinutes()}:${date.getSeconds()}`;
