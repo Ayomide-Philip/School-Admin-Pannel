e775095373912f34445030e17885f8eed05bfe03
A-pilip <areoayomide2008@gmail.com>
2024-09-23 00:17:11 +0100

    updating testimonial.js

--- script/testimonial.js
+++ script/testimonial.js
@@ -1,9 +1,11 @@
+
 setInterval(getTime, 1000);
 
 function getTime() {
   const date = new Date();
   var seconds = date.getSeconds();
   var minutes = date.getMinutes();
+  var hour = date.getHours();
   if (seconds < 10) {
     seconds = `0${seconds}`;
   } else {
@@ -16,9 +18,14 @@ function getTime() {
     minutes = `${minutes}`;
   }
 
+  if (hour < 10) {
+    hour = `0${hour}`;
+  } else {
+    hour = `${hour}`;
+  }
   document.getElementById(
     "current-time"
-  ).innerHTML = `${new Date().getHours()}:${minutes}:${seconds}`;
+  ).innerHTML = `${hour}:${minutes}:${seconds}`;
 }
 
 const date = new Date().getHours();
