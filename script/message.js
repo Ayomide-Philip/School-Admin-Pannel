644dd3a6ce6e8afd4366c69b60d47bdcbad740ab
A-pilip <areoayomide2008@gmail.com>
2024-09-23 00:15:47 +0100

    updating my message.js

--- script/message.js
+++ script/message.js
@@ -1,11 +1,10 @@
-console.log(name);
-
 setInterval(getTime, 1000);
 
 function getTime() {
   const date = new Date();
   var seconds = date.getSeconds();
   var minutes = date.getMinutes();
+  var hour = date.getHours();
   if (seconds < 10) {
     seconds = `0${seconds}`;
   } else {
@@ -18,17 +17,12 @@ function getTime() {
     minutes = `${minutes}`;
   }
 
+  if (hour < 10) {
+    hour = `0${hour}`;
+  } else {
+    hour = `${hour}`;
+  }
   document.getElementById(
     "current-time"
-  ).innerHTML = `${new Date().getHours()}:${minutes}:${seconds}`;
-}
-
-const date = new Date().getHours();
-
-if (0 <= date && date <= 11) {
-  document.getElementById("day-time").innerHTML = `Good Morning Admin`;
-} else if (12 <= date && date <= 18) {
-  document.getElementById("day-time").innerHTML = `Good Afternoon Admin`;
-} else if (19 <= date && date <= 23) {
-  document.getElementById("day-time").innerHTML = `Good Evening Admin`;
+  ).innerHTML = `${hour}:${minutes}:${seconds}`;
 }
