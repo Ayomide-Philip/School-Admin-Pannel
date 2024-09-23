8bef20c21f88cf23fea561bc40c01829fc341946
A-pilip <areoayomide2008@gmail.com>
2024-09-23 11:10:31 +0100

    updating my js file

--- script/post-testimonial.js
+++ script/post-testimonial.js
@@ -36,3 +36,49 @@ if (0 <= date && date <= 11) {
 } else if (19 <= date && date <= 23) {
   document.getElementById("day-time").innerHTML = `Good Evening Admin`;
 }
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", (event) => {
+  var inputedName = document.getElementById("name").value;
+  var inputedMessage = document.getElementById("message").value;
+  
+  inputedMessage = inputedMessage.trim();
+  inputedName = inputedName.trim();
+
+  if (inputedName.length === 0) {
+    event.preventDefault();
+    document.getElementById("name-error").style.display = "block";
+    document.getElementById("name-error").innerText =
+      "Enter your name into this field";
+  } else {
+    if (inputedName.indexOf(" ") < 0) {
+      event.preventDefault();
+      document.getElementById("name-error").style.display = "block";
+      document.getElementById("name-error").innerText =
+        "You only Inputed your Surname or Lastname";
+    } else {
+      document.getElementById("name-error").style.display = "none";
+    }
+  }
+
+  if (inputedMessage.length === 0) {
+    event.preventDefault();
+    document.getElementById("message-error").style.display = "block";
+    document.getElementById("message-error").innerText =
+      "Input your message into the field.";
+  } else {
+    if (inputedMessage.length < 20) {
+      event.preventDefault();
+      console.log(inputedMessage.length);
+      document.getElementById("message-error").style.display = "block";
+      document.getElementById(
+        "message-error"
+      ).innerText = `You need at least 20 letter word, it remains ${
+        20 - inputedMessage.length
+      }.`;
+    } else {
+      document.getElementById("message-error").style.display = "none";
+    }
+  }
+});
